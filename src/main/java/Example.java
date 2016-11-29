2514e5d1357180c509acce14eb546e9a3d61559d
davidbejar <david.bejar@gmail.com>
2016-11-29 21:00:58 +0700

    Change output from String to JSON

--- src/main/java/Example.java
+++ src/main/java/Example.java
@@ -10,12 +10,65 @@ import org.springframework.web.bind.annotation.RestController;
 @EnableAutoConfiguration
 public class Example {
 
+	/**
+	 * inner class for output json
+	 *
+	 * @author davidbejar
+	 *
+	 */
+	class SpringPojo {
+		/**
+		 * @return the springBootVersion
+		 */
+		public String getSpringBootVersion() {
+			return springBootVersion;
+		}
+
+		/**
+		 * @return the springFrameworkVersion
+		 */
+		public String getSpringFrameworkVersion() {
+			return springFrameworkVersion;
+		}
+
+		/**
+		 * @return the tomcatServerInfo
+		 */
+		public String getTomcatServerInfo() {
+			return tomcatServerInfo;
+		}
+
+		/**
+		 * @return the tomcatVersion
+		 */
+		public String getTomcatVersion() {
+			return tomcatVersion;
+		}
+
+		/**
+		 * @return the tomcatBuilt
+		 */
+		public String getTomcatBuilt() {
+			return tomcatBuilt;
+		}
+
+		String springBootVersion;
+		String springFrameworkVersion;
+		String tomcatServerInfo;
+		String tomcatVersion;
+		String tomcatBuilt;
+
+	}
+
 	@RequestMapping("/")
-	String home() {
-		return new StringBuilder("Running Spring Boot version: ").append(SpringBootVersion.getVersion())
-				.append(" , Spring Framework version: ").append(SpringVersion.getVersion())
-				.append(" and Tomcat Server info, number and built: ").append(ServerInfo.getServerInfo()).append(" ")
-				.append(ServerInfo.getServerNumber()).append(" ").append(ServerInfo.getServerBuilt()).toString();
+	SpringPojo home() {
+		SpringPojo springPojo = new SpringPojo();
+		springPojo.springBootVersion = SpringBootVersion.getVersion();
+		springPojo.springFrameworkVersion = SpringVersion.getVersion();
+		springPojo.tomcatServerInfo = ServerInfo.getServerInfo();
+		springPojo.tomcatVersion = ServerInfo.getServerNumber();
+		springPojo.tomcatBuilt = ServerInfo.getServerBuilt();
+		return springPojo;
 	}
 
 	public static void main(String[] args) throws Exception {
