3818752e7fdc1bdda87ffd54f3817bc61499cbac
davidbejar <david.bejar@gmail.com>
2016-11-29 12:39:56 +0700

    Print Spring Boot version instead of Hello World message
    Update imports too.

--- src/main/java/Example.java
+++ src/main/java/Example.java
@@ -1,19 +1,20 @@
-import org.springframework.boot.*;
-import org.springframework.boot.autoconfigure.*;
-import org.springframework.stereotype.*;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootVersion;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @EnableAutoConfiguration
 public class Example {
 
-  @RequestMapping("/")
-  String home() {
-    return "Hello World!";
-  }
+	@RequestMapping("/")
+	String home() {
+		return new StringBuilder("Running Spring Boot version: ").append(SpringBootVersion.getVersion()).toString();
+	}
 
-  public static void main(String[] args) throws Exception {
-    SpringApplication.run(Example.class, args);
-  }
-  
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(Example.class, args);
+	}
+
 }
